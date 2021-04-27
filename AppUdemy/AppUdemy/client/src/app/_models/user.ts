0dbb6a6055466dce8661f5eee549faa7dbf43fcf
EricPastorGC <eric.pastor@grupocastilla.es>
2021-04-27 08:48:48 +0200

    Seccion 9 terminada

--- AppUdemy/AppUdemy/client/src/app/_models/user.ts
+++ AppUdemy/AppUdemy/client/src/app/_models/user.ts
@@ -2,3 +2,9 @@ export interface User{
     username: string;
     token: string;
 }
+
+//let data: number | string = 42;  //en typescript se pueden aplicar diferentes tipos de variable a una misma variable
+
+//data = "8"; //sin el doble tipo de variable no podriamos aplicar aqui un string porque cuando hemos inicializado se le ha dado un number
+
+//en una interfaz si se usa ? en una variable esa variable no sera obligatoria a la hora de crear una interfaz de ese tipo 
